82f91b319fb7f00b0796277ced30d395abd31950
caiolarroza <caiolarroza@hotmail.com>
2019-08-08 00:13:55 +0000

    Adicionando organização de pastas segundo o ITCSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/settings/colors.css';
 import './styles/generic/reset.css';
 import './styles/elements/base.css';
-import './styles/settings/colors.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
